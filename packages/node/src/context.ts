7d788718523ac91980f7a9db7484ce5a7a2a58a7
ayecue <soerenwehmeier@googlemail.com>
2024-10-29 21:26:36 +0100

    update core

--- packages/node/src/context.ts
+++ packages/node/src/context.ts
@@ -2,16 +2,19 @@ import { createConnection, ProposedFeatures } from 'vscode-languageserver/node';
 
 import { CoreContext, DocumentManager } from 'miniscript-languageserver-core';
 import { FileSystem } from './fs';
+import { DocumentMerger } from 'miniscript-languageserver-core/dist/helper/document-merger';
 
 export class NodeContext extends CoreContext {
   readonly connection: ReturnType<typeof createConnection>;
   readonly fs: FileSystem;
   readonly documentManager: DocumentManager;
+  readonly documentMerger: DocumentMerger;
 
   constructor() {
     super();
 
     this.documentManager = new DocumentManager().setContext(this);
+    this.documentMerger = new DocumentMerger();
     this.connection = createConnection(ProposedFeatures.all);
     this.fs = new FileSystem(this);
   }
