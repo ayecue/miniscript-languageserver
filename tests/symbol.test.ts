75db746b78742f4ff9574393b191ee0efd622ad7
ayecue <soerenwehmeier@googlemail.com>
2024-11-16 18:36:03 +0100

    add symbol test

---
+++ tests/symbol.test.ts
@@ -0,0 +1,44 @@
+import { suite, test } from 'mocha';
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import { getDocUri, activate } from './helper';
+
+async function executeDocumentSymbolProvider(
+  docUri
+) {
+  await activate(docUri);
+
+  return await vscode.commands.executeCommand<vscode.SymbolInformation[]>(
+    'vscode.executeDocumentSymbolProvider',
+    docUri
+  );
+}
+
+suite('DocumentSymbolProvider', () => {
+  suite('default', () => {
+    test('should provide locations', async () => {
+      const documentUri = getDocUri('default.ms');
+      const result = await executeDocumentSymbolProvider(documentUri);
+
+      assert.strictEqual(result.length, 10);
+    });
+  });
+
+  suite('class', () => {
+    test('should provide locations', async () => {
+      const documentUri = getDocUri('class.ms');
+      const result = await executeDocumentSymbolProvider(documentUri);
+
+      assert.strictEqual(result.length, 106);
+    });
+  });
+
+  suite('invalid code', () => {
+    test('should provide locations', async () => {
+      const documentUri = getDocUri('invalid-chunk.src');
+      const result = await executeDocumentSymbolProvider(documentUri);
+
+      assert.strictEqual(result.length, 2);
+    });
+  });
+});
