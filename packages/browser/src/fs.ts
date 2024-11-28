308e4140ae8f7e9ee1f822912a2f81b967fd4682
ayecue <soerenwehmeier@googlemail.com>
2024-11-28 23:15:39 +0100

    update core

--- packages/browser/src/fs.ts
+++ packages/browser/src/fs.ts
@@ -7,7 +7,8 @@ import LRUCache from "lru-cache";
 import { IContext, IFileSystem, LanguageId } from "miniscript-languageserver-core";
 
 export enum FileSystemRequest {
-  FileContent = 'read-file'
+  FileContent = 'read-file',
+  FindFiles = 'find-files'
 }
 
 export class FileSystem extends EventEmitter implements IFileSystem {
@@ -42,6 +43,30 @@ export class FileSystem extends EventEmitter implements IFileSystem {
     return uris.find(folderUri => source.path.startsWith(folderUri.path)) || null;
   }
 
+  private async requestFindFiles(include: string, exclude?: string): Promise<string[]> {
+    const method = `custom/${FileSystemRequest.FindFiles}`;
+
+    try {
+      return await this._context.connection.sendRequest(method, JSON.stringify({ include, exclude }));
+    } catch (err) {
+      console.error(`Cannot fetch workspace files! Maybe the client does not support ${method}`, err);
+      return [];
+    }
+  }
+
+  async getWorkspaceRelatedFiles(): Promise<URI[]> {
+    const configuration = this._context.getConfiguration();
+    const fileExtensions = configuration.fileExtensions;
+    const exclude = configuration.typeAnalyzer.exclude;
+    const filePaths = await this.requestFindFiles(`**/*.{${fileExtensions.join(',')}}`, exclude);
+    return filePaths.map((it) => URI.parse(it));
+  }
+
+  async getWorkspaceFileUris(pattern: string, exclude?: string): Promise<URI[]> {
+    const filePaths = await this.requestFindFiles(pattern, exclude);
+    return filePaths.map((it) => URI.parse(it));
+  }
+
   async findExistingPath(...uris: string[]): Promise<string | null> {
     if (uris.length === 0) {
       return null;
@@ -63,7 +88,7 @@ export class FileSystem extends EventEmitter implements IFileSystem {
     const method = `custom/${FileSystemRequest.FileContent}`;
 
     try {
-      return await this._context.connection.sendRequest(method, fileUri.toString());
+      return await this._context.connection.sendRequest(method, JSON.stringify({ uri: fileUri.toString() }));
     } catch (err) {
       console.error(`Cannot fetch text document! Maybe the client does not support ${method}`, err);
       return null;
